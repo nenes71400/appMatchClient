102cb4544c4e456e07e5a5748db62302b44e4172
Alexis Desvignes <a.desvignes@aeb-autun.com>
2022-04-04 08:06:16 +0200

    Next

--- App.tsx
+++ App.tsx
@@ -1,29 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View, Text} from 'react-native';
 //import { StyleSheet, Text, View, Image } from 'react-native';
-import { RootNavigator } from './navigation/RootNavigator';
-
-//import { ClubsScreen } from './screens/Clubs'
-/*
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: `http://localhost:5000/clubs/read`
-})
-*/
-
+//import { RootNavigator } from './navigation/RootNavigator';
+import { RootNavigator } from './src/navigation/RootNavigator';
 
 //export class App extends Component {
+
 export default function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
-      <StatusBar style="auto" />
     </NavigationContainer>
   )
 };
+
+/*
+      <Tabs/>
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+      <StatusBar style="auto" />
+      <Tabs/>
+    </NavigationContainer>
+  )
+};
+*/
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red', //#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 
 
@@ -40,35 +52,28 @@ export default function App() {
       })
   }
   return <ClubsScreen />
-*/
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerStyle: {
+    marginBottom: 24,
+    fontSize: 22,
+    fontWeight: 'bold',
   },
-//  headerStyle: {
-//    marginBottom: 24,
-//    fontSize: 22,
-//    fontWeight: 'bold',
-//  },
-//  red: {
-//    color: 'red',
-//  },
-//
+  red: {
+    color: 'red',
+  },
+
 });
 
-//      <Text
-//        style={[styles.headerStyle, styles.red]}
-//        >
-//      appTournoi!
-//      </Text>
-//      <Image
-//        style={{ width: 200, height: 400}}
-//        source={{
-//          uri: "http://www.rugby-bourgogne.com/fichiers/2014/10/100_7454.jpg"
-//          }}
-//      />
-//
+      <Text
+        style={[styles.headerStyle, styles.red]}
+        >
+      appTournoi!
+      </Text>
+      <Image
+        style={{ width: 200, height: 400}}
+        source={{
+          uri: "http://www.rugby-bourgogne.com/fichiers/2014/10/100_7454.jpg"
+          }}
+      />
+
+*/
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,41 @@
 module.exports = function(api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [ 'module-resolver', {
+        root: ['./src'],
+        alias: {
+          '~': './src',
+          assets: './assets',
+          components: './src/components',
+          modules: './src/modules',
+          /*
+          lib: './src/lib',
+          types: './src/types',
+          constants: './src/constants',
+          */
+        },
+      },],
+    ],
+  };
+};
+/*
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // Add following to babel.config.js file
+    //module.exports = {
+      //presets: ['module:metro-react-native-babel-preset'],
+      plugins: [
+        ['module-resolver', {
+          root: ['./src'],
+          alias: { '~': './src',
+          },
+        },],
+      ],
+    //};
   };
 };
+*/
